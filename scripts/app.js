b4b080ac867834d4592b4718173797a6c91bfc8d
pgm-youritack <youri.tack@student.arteveldehs.be>
2023-01-24 22:28:48 +0100

    added aframe quote generator

--- scripts/app.js
+++ scripts/app.js
@@ -2,15 +2,20 @@ import '../style/main.scss';
 import menus from './menus';
 import animal from './animal';
 import teacher from './teacher';
-const initApp = () => {};
-try {
-  menus();
-} catch (error) {}
-try {
-  animal();
-} catch (error) {}
-try {
-  teacher();
-} catch (error) {}
+import quotes from './quotes.js';
+const initApp = () => {
+  try {
+    menus();
+  } catch (error) {}
+  try {
+    animal();
+  } catch (error) {}
+  try {
+    teacher();
+  } catch (error) {}
+  try {
+    quotes();
+  } catch (error) {}
+};
 
 document.addEventListener('DOMContentLoaded', initApp);
---
+++ scripts/quotes.js
@@ -0,0 +1,35 @@
+const quotes = () => {
+const backgrounds=[
+    'mountain.jpg',
+    'space.jpg',
+    'tree.jpg',
+    'waterfall.jpg'
+]
+const quotes=[
+    'It’s not a bug; it’s an undocumented feature',
+    'Code is like humor. When you have to explain it, it’s bad',
+    'Software is like sex: it’s better when it’s free',
+    'Software and cathedrals are much the same — first we build them, then we pray'
+]
+const background = backgrounds[Math.floor(Math.random()*backgrounds.length)];
+const randomquote = quotes[Math.floor(Math.random()*quotes.length)];
+const image = document.querySelector('#my-image');
+const quote = document.querySelector('#quote');
+const refresh = document.querySelector('#refresh');
+image.src=`./public/backgrounds/${background}`
+quote.setAttribute('value',randomquote)
+
+refresh.addEventListener('click',()=>{
+    const background = backgrounds[Math.floor(Math.random()*backgrounds.length)];
+const randomquote = quotes[Math.floor(Math.random()*quotes.length)];
+const image = document.querySelector('#my-image');
+const quote = document.querySelector('#quote');
+console.log(image);
+image.src=`./public/backgrounds/${background}`
+quote.setAttribute('value',randomquote)
+})
+
+
+  };
+  export default quotes;
+  
